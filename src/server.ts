fce5cfe550c54f6acdfb61388832ff6fa5dcab2f
Elias Lazcano Castro Neto <elias304162@gmail.com>
2022-11-02 19:44:41 -0400

    Aplicando normas do CORS para funcionar
    Configura o uso das regras CORS no Fastify, fazendo ele atender requisições do método OPTIONS e tornando isso configurável.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 
 // Cliente de banco de dados
@@ -10,6 +11,10 @@ const prisma = new PrismaClient({
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
+  });
+
+  await fastify.register(cors, {
+    origin: true, //Aceita de qualquer origem
   });
 
   fastify.get('/pools/count', async () => {
